89396f0cf0b7b61c4eab52c28aeb2c57eefc466e
Oliver <117604613+OliverHu726@users.noreply.github.com>
2023-12-14 16:52:46 -0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,50 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+import sklearn
+
+# Add import statements for Preprocessor and columns if necessary
+
+model_url = 'https://raw.github.com/OliverHu726/ML_in_FRE_Project_App/main/model.pkl'
+model_response = requests.get(model_url)
+loaded_model = pickle.load(BytesIO(model_response.content))
+
+# title
+st.title("Predictions based on stock sentiment index")
+st.write('*By feeding in the sentiment index, the model can predict the future rise and fall of the stock!* :sunglasses:')
+
+Subjectivity = st.number_input('Subjectivity 0~1', 0.0)
+Polarity = st.number_input('Polarity -0.5~0.5', -0.4)
+compound = st.number_input('compound -1~1', -0.997)
+neg = st.number_input('neg 0~1', 0.0)
+pos = st.number_input('pos 0~1', 0.0)
+neu = st.number_input('neu 0~1', 0.532)
+
+# Make sure to define 'columns' with the correct order of features used during training
+columns = ['Subjectivity', 'Polarity', 'compound', 'neg', 'pos', 'neu']
+
+def predict():
+    row = np.array([Subjectivity, Polarity, compound, neg, pos, neu])
+    X = pd.DataFrame([row], columns=columns)
+    prediction = model.predict(X)[0]
+    if prediction == 1:
+        st.success('Stocks have a higher probability of going up :thumbsup:')
+    else:
+        st.error('Stocks have a higher probability of going down :thumbsdown:')
+
+# Fix the on_click parameter
+st.button('Predict', on_click=predict)
+
+st.image('wordcloud.png')
+st.image('vi.png')
+
+
+
+
+
+
+
+
+
 
